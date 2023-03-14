55884dfae5127bfb91f592ba7c859a3126b1b2f0
AJ12396 <vilmathianna@gmail.com>
2023-03-14 17:31:50 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var objects = []
+var status = ""
+var video = ""
+
+function preload() {
+    video = createVideo("video.mp4")
+}
+
+function setup() {
+    canvas = createCanvas(480, 380)
+}
+
+function draw() {
+    
+}
