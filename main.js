6579cc7f3cda62a6df6be7bad317bce0eccfef1d
AJ12396 <vilmathianna@gmail.com>
2023-03-16 17:16:52 -0300

    Novo!

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var objects = []
 var status = ""
 var video = ""
+var objectDetector
 
 function preload() {
     video = createVideo("video.mp4")
@@ -8,8 +9,22 @@ function preload() {
 
 function setup() {
     canvas = createCanvas(480, 380)
+    canvas.center()
+    video.hide()
+}
+function draw() {
+    image(video, 0, 0 , 480, 380)
 }
 
-function draw() {
-    
+function start() {
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded)
+    document.getElementById("status").innerHTML = "Status: detectando objetos"
+}
+
+function modelLoaded() {
+    console.log("O modelo foi carregado!")
+    status = true
+    video.loop()
+    video.speed(1)
+    video.volume(0)
 }
