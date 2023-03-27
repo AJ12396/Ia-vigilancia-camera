ee073082dfa87deeb4ec80a8ad2b63e9355dbca7
AJ12396 <vilmathianna@gmail.com>
2023-03-27 17:32:56 -0300

    ...

--- main.js
+++ main.js
@@ -12,9 +12,6 @@ function setup() {
     canvas.center()
     video.hide()
 }
-function draw() {
-    image(video, 0, 0 , 480, 380)
-}
 
 function start() {
     objectDetector = ml5.objectDetector("cocossd", modelLoaded)
@@ -27,4 +24,30 @@ function modelLoaded() {
     video.loop()
     video.speed(1)
     video.volume(0)
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(results)
+        objects = results
+    }
+}
+
+function draw() {
+    image(video, 0, 0 , 480, 380)
+    if (status!="") {
+        objectDetector.detect(video, gotResult)
+        for (let i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML  = "Status: objetos detectados" 
+            document.getElementById("numberOfObjects").innerHTML  = "Quantidade de objetos detectados: " + objects.length
+            fill("red")
+            percent = floor(objects[i].confidence * 100)
+            text(objects[i].label + " " + percent + "%", objects[i].x +15, objects[i].y +15)
+            noFill()
+            stroke("red")
+            rect(objects[i].x , objects[i].y, objects[i].width, objects[i].height)
+        }
+    }
 }
